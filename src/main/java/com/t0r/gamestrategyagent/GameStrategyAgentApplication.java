16691de88c386619dca72d47381071dff6b50218
Towerrrr <1611993104@qq.com>
2025-12-23 20:00:04 +0800

    feat: 项目初始化

---
+++ src/main/java/com/t0r/gamestrategyagent/GameStrategyAgentApplication.java
@@ -0,0 +1,13 @@
+package com.t0r.gamestrategyagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStrategyAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStrategyAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/t0r/gamestrategyagent/GameStrategyAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t0r.gamestrategyagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStrategyAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
